2401a3f2bccc7087d8aeb2f3dde494dda1e527e1
adpranavb2000@gmail.com <adpranavb2000@gmail.com>
2020-07-23 14:15:11 +0530

    changes to remote.py

--- remote.py
+++ remote.py
@@ -1,30 +1,3 @@
-"""
-import asyncio
-from rtcbot import Websocket, RTCConnection, CVCamera
-
-cam = CVCamera()
-conn = RTCConnection()
-conn.video.putSubscription(cam)
-
-# Connect establishes a websocket connection to the server,
-# and uses it to send and receive info to establish webRTC connection.
-async def connect():
-    ws = Websocket("https://rtcbot.dev/InventoTest12")
-    remoteDescription = await ws.get()
-    robotDescription = await conn.getLocalDescription(remoteDescription)
-    ws.put_nowait(robotDescription)
-    print("Started WebRTC")
-    await ws.close()
-
-
-asyncio.ensure_future(connect())
-try:
-    asyncio.get_event_loop().run_forever()
-finally:
-    cam.close()
-    conn.close()
-
-"""
 import asyncio
 from rtcbot import Websocket, RTCConnection, CVCamera, CVDisplay, Speaker, Microphone
 
@@ -61,7 +34,7 @@ def onMessage(msg):  # Called when each message is sent
 # Connect establishes a websocket connection to the server,
 # and uses it to send and receive info to establish webRTC connection.
 async def connect():
-    ws = Websocket("https://f5b9ee71723e.ngrok.io/InventoTest12")
+    ws = Websocket("https://2d2dda3cbc5e.ngrok.io/InventoTest12")
     remoteDescription = await ws.get()
     robotDescription = await conn.getLocalDescription(remoteDescription)
     ws.put_nowait(robotDescription)
